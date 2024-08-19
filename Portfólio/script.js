1dfe963d968d1eda3b70f1dac39072d198a00719
Leonardo Igor <115428616+leosportt@users.noreply.github.com>
2024-08-19 08:19:02 -0300

    Update script.js

--- Portfólio/script.js
+++ Portfólio/script.js
@@ -13,3 +13,16 @@ function typeWriter() {
 window.onload = function() {
     typeWriter();
 };
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
